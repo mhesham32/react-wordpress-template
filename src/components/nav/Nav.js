9cd427559be60d0104314e7f191c4bd52c3593e2
mhesham32 <m.hesham.32@live.com>
2018-07-19 22:42:15 +0300

    add the loading nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,90 +1,45 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import NavItem from './NavItem';
+import NavLoading from './NavLoading';
 import { getIsFetching, getRoutesData } from '../../reducers/catrgoriesReducer';
 import { fetchCateogries } from '../../actions/categories';
-
-// const routes = [
-//   {
-//     dropdown: false,
-//     name: 'Home',
-//   },
-//   {
-//     dropdown: true,
-//     name: 'tech',
-//     links: ['apple', 'Microsoft', 'Google', 'Samsung'],
-//   },
-//   {
-//     dropdown: true,
-//     name: 'Science',
-//     links: ['space', 'Energy', 'Health'],
-//   },
-//   {
-//     dropdown: false,
-//     name: 'Culture',
-//   },
-//   {
-//     dropdown: true,
-//     name: 'Cars',
-//     links: ['BMW', 'Ferrari', 'Mass Transit'],
-//   },
-// ];
+import NavBody from './NavBody';
 
 class Nav extends Component {
+  static propTypes = {
+    fetchRoutes: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    routes: PropTypes.array.isRequired,
+    location: PropTypes.object.isRequired,
+  };
+
   componentDidMount() {
     this.props.fetchRoutes();
   }
 
   render() {
-    if (this.props.isFetching && this.props.routes.length === 0) {
-      return <h1>Loading....</h1>;
-    }
+    const { isFetching, routes, location } = this.props;
+
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          React Blog
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            {this.props.routes.map(route => {
-              if (
-                this.props.location.pathname.includes(route.name) ||
-                (this.props.location.pathname === '/' && route.name === 'Home')
-              ) {
-                return <NavItem {...route} key={route.name} active />;
-              }
-              return <NavItem {...route} key={route.name} active={false} />;
-            })}
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </nav>
+      <NavBody title="React Blog">
+        {isFetching && routes.length === 0 ? (
+          <NavLoading />
+        ) : (
+          routes.map(route => {
+            if (
+              location.pathname.includes(route.name) ||
+              (location.pathname === '/' && route.name === 'Home')
+            ) {
+              return <NavItem {...route} key={route.name} active />;
+            }
+            return <NavItem {...route} key={route.name} active={false} />;
+          })
+        )}
+      </NavBody>
     );
   }
 }
---
+++ src/components/nav/NavLoading.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const NavLoading = () => (
+  <Fragment>
+    <li className="nav-loading__item nav-item" />
+    <li className="nav-loading__item nav-item" />
+    <li className="nav-loading__item nav-item" />
+    <li className="nav-loading__item nav-item" />
+  </Fragment>
+);
+
+export default NavLoading;
